4e06c61a7cf65a8d277074ca6b228a14ffe05c4e
Zahid Mzhmm <zahidmzhmm@gmail.com>
2022-08-21 00:06:00 +0600

    Work Done

--- app/config.php
+++ app/config.php
@@ -1,8 +1,23 @@
 <?php
 
-define("SITE_URL", 'http://summerworkprograms.com/');
+define("SITE_URL", 'https://summerworkprograms.com/');
 define("ADMIN_EMAIL", "info@summerworkprograms.com");
-//define("ADMIN_EMAIL2", "admin@summerworkprograms.com");
+/*
+define("ADMIN_EMAIL2", "admin@summerworkprograms.com");
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'summerwo_main');
+define('DB_PASS', 'summerwo_main@12345S');
+define('DB_NAME', 'summerwo_swp');
+
+define("SMTP_HOST", "dallas152.arvixeshared.com");
+define("SMTP_USER", "donotreply@summerworkprograms.com");
+define("SMTP_EMAIL", "donotreply@summerworkprograms.com");
+define("SMTP_PASS", "dlwcvnhycpsbrjll");
+define("SMTP_DEBUG", 0);
+define("SMTP_PORT", 465);
+*/
+
 define("ADMIN_EMAIL2", "zahidmzhmm@gmail.com");
 
 define('DB_HOST', 'localhost');
@@ -18,4 +33,3 @@ define("SMTP_DEBUG", 0);
 define("SMTP_PORT", 465);
 
 define('PS_KEY', 'sk_test_9c5da4397931a6e1b3084d45029fceb1f6088780');
-?>
