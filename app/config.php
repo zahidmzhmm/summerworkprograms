534231d104c6a06a36ee3517f13c34adf68b21f0
Zahid Mzhmm <zahidmzhmm@gmail.com>
2022-08-22 19:19:52 +0600

    Config Changed

--- app/config.php
+++ app/config.php
@@ -6,14 +6,14 @@ define("ADMIN_EMAIL", "info@summerworkprograms.com");
 define("ADMIN_EMAIL2", "admin@summerworkprograms.com");
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'summerwo_main');
-define('DB_PASS', 'summerwo_main@12345S');
-define('DB_NAME', 'summerwo_swp');
+define('DB_USER', 'summerprog_main');
+define('DB_PASS', 'Summer@12345');
+define('DB_NAME', 'summerprog_main');
 
-define("SMTP_HOST", "dallas152.arvixeshared.com");
+define("SMTP_HOST", "premium259.web-hosting.com");
 define("SMTP_USER", "donotreply@summerworkprograms.com");
 define("SMTP_EMAIL", "donotreply@summerworkprograms.com");
-define("SMTP_PASS", "dlwcvnhycpsbrjll");
+define("SMTP_PASS", "Summer@12345");
 define("SMTP_DEBUG", 0);
 define("SMTP_PORT", 465);
 */
