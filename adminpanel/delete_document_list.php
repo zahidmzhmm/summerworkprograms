b00dae34eb328b63363b6f148503134176da0acd
Zahid Mzhmm <zahidmzhmm@gmail.com>
2022-09-30 11:53:08 +0600

    Work Finished

--- adminpanel/delete_document_list.php
+++ adminpanel/delete_document_list.php
@@ -1,36 +1,17 @@
 <?php
-include "includes/includes.php";
-
-$conn = include('../mysql-db.php');
-
+include "../app/main.php";
 $id = $_GET["id"];
 $count = $_GET["count"];
 
-$member = Member::find($id);
-$list = unserialize($member->support_document_list);
+$member = app\Sql::Select_single("SELECT support_document_list FROM tbl_member where users_id='$id'");
+if ($member) {
+    $member = (object)$member;
+    $list = unserialize($member->support_document_list);
+    array_splice($list, $count - 1, 1);
+    $list = serialize($list);
+    $medoo = new Medoo\Medoo($database);
+    $medoo->update('tbl_member', ['support_document_list' => $list], ['users_id' => $id]);
+}
+header('Location: home.php?modules=support_documents&action=view&id=' . $id);
 
-array_splice($list, $count-1, 1); 
-
-$list = serialize($list);
-
-$member->support_document_list = $list;
-$member->save();
-
-// $conn = new mysqli($conn["servername"], $conn["username"], $conn["password"], $conn["dbname"]);
-
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// } 
-
-// $sql = "UPDATE support_document_list SET data='$list' WHERE id=1";
-
-// if ($conn->query($sql) === TRUE) {
-//     echo "Record updated successfully";
-// } else {
-//     echo "Error updating record: " . $conn->error;
-// }
-
-// $conn->close();
-
-header('Location: home.php?modules=support_documents&action=view&id='.$id);
 ?>
--- adminpanel/delete_upload_list.php
+++ adminpanel/delete_upload_list.php
@@ -1,36 +1,16 @@
 <?php
-include "includes/includes.php";
-
-$conn = include('../mysql-db.php');
-
+include "../app/main.php";
 $id = $_GET["id"];
 $count = $_GET["count"];
 
-$member = Member::find( $id );
-// $list = UploadDocumentList::first();
-$list = unserialize($member->upload_document_list);
-array_splice($list, $count-1, 1); 
-
-$list = serialize($list);
-
-$member->upload_document_list = $list;
-$member->save();
-
-// $conn = new mysqli($conn["servername"], $conn["username"], $conn["password"], $conn["dbname"]);
-
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// } 
-
-// $sql = "UPDATE upload_document_list SET data='$list' WHERE id=1";
-
-// if ($conn->query($sql) === TRUE) {
-//     echo "Record updated successfully";
-// } else {
-//     echo "Error updating record: " . $conn->error;
-// }
-
-// $conn->close();
-
+$member = app\Sql::Select_single("SELECT upload_document_list FROM tbl_member where users_id='$id'");
+if ($member) {
+    $member = (object)$member;
+    $list = unserialize($member->upload_document_list);
+    array_splice($list, $count-1, 1);
+    $list = serialize($list);
+    $medoo = new Medoo\Medoo($database);
+    $medoo->update('tbl_member', ['upload_document_list' => $list], ['users_id' => $id]);
+}
 header('Location: home.php?modules=upload_documents&action=view&id='.$id);
 ?>
