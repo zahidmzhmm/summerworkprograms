d74af69c8454ddff2c0b46d2ec306d13c3a2e95c
Zahid Mzhmm <zahidmzhmm@gmail.com>
2023-07-21 17:15:22 +0600

    Update config.php

--- app/config.php
+++ app/config.php
@@ -7,14 +7,14 @@ define("ADMIN_EMAIL2", "admin@summerworkprograms.com");
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'summerprog_main');
-define('DB_PASS', 'Summer@12345');
+define('DB_PASS', '');
 define('DB_NAME', 'summerprog_main');
 
 define("SMTP_HOST", "premium259.web-hosting.com");
 define("SMTP_USER", "donotreply@summerworkprograms.com");
 define("SMTP_EMAIL", "donotreply@summerworkprograms.com");
-define("SMTP_PASS", "@RemmuS@247#");
+define("SMTP_PASS", "");
 define("SMTP_DEBUG", 0);
 define("SMTP_PORT", 465);
 
-define('PS_KEY', 'sk_live_6a375e425e80dd596825a1bb4c08d56d75347784');
+define('PS_KEY', '');
